4a343830de800c6447635b9303daaceb65ae1b56
DarrenDoan <doandarren95@gmail.com>
2024-02-12 17:46:38 +1100

    moved files around

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const html_route = require('./routes/html-route');
+const api_route= require('./routes/api-route');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware will run from top to bottom
+app.use(express.urlencoded({ extended: false }));
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+// Static middleware pointing to the public folder
+app.use(express.static("public"));
+app.use(html_route)
+app.use(api_route)
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
